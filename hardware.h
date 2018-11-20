5cf8adf31264571126d2a11b2bf3577150c6546b
Jose Guerra Carmenate <joseguerracarmenate@gmail.com>
2018-11-19 22:26:12 -0500

    Added pending files

---
+++ hardware.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef _HARDWARE_H
+#define	_HARDWARE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+#ifdef PIC_TRAINER_WAR_MINI
+ #include "hardware/hardware_war_mini.h"
+#endif
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
